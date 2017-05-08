7b4157bbc14d317337952a1572b675bba94e0e9f
grhaonan <liucong.haonan@gmail.com>
2017-05-08 12:14:14 +0800

    remote double quotes for object keys

--- Project_Javascript_Basic/js/resumeBuilder.js
+++ Project_Javascript_Basic/js/resumeBuilder.js
@@ -1,40 +1,39 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
 
 
-
-
-
-// var formattedContact = HTMLcontactGeneric.replace("%data%", "18601163385");
-// $("#header").append(formattedContact);
 
 
 
 // Data Parts
 var work = {
-    "jobs":[
-        {
-            "employer": "Ericsson",
-            "title": "Core Network Engineer",
-            "location": "Beijing",
-            "dates": "1st March 2010 - 1st March 2013",
-            "description": "Network Integration bla bla bla"
-        },
-
-        {
-            "employer": "Ericsson",
-            "title": "Project Manager",
-            "location": "Beijing",
-            "dates": "2nd March 2013 - 8th May 2015",
-            "description": "Manage newwork bla bla bla"
-        }
+    // Recoding work related information
+    jobs:[
+    {
+        employer: "Ericsson",
+        title: "Core Network Engineer",
+        location: "Beijing",
+        dates: "1st March 2010 - 1st March 2013",
+        description: "The Network Engineer performs design, optimization and audit of a network to meet the customer requirements." +
+        "The Network Engineer role is applicable for design & optimization of RAN, BBA, Transmission, Core, OM and Services Networks." +
+        "The Network Engineer can be engaged in the whole process from pre-sales of services and networks to service delivery and acceptance."
+    },
+    {
+        employer: "Ericsson",
+        title: "Project Manager",
+        location: "Beijing",
+        dates: "2nd March 2013 - 8th May 2015",
+        description: "Defining the project scope together with Product and System Management, secure the necessary resources with Line Management," +
+        "and be responsible for project time plan, budget planning/costs, quality and risks by planning, monitoring and controlling all activities to" +
+        "get your project successfully executed within time, scope and budget."
+    }
     ],
+    // function to append work related information to html
     display: function (){
         $("#workExperience").append(HTMLworkStart);
-         work.jobs.forEach (function(element){
+        work.jobs.forEach (function(element){
             // create new div for work experience
-
             // concat employer name and title
             var formattedEmpolyer = HTMLworkEmployer.replace("%data%", element.employer);
             var formattedTitle = HTMLworkTitle.replace("%data%", element.title);
@@ -53,60 +52,60 @@ var work = {
         });
     }
 };
-
 var projects = {
-    "projects":[
-                    {
-                        "title": "Volvo Auto Driving",
-                        "dates": "2010-05-01",
-                        "description": "Testing new auto driving framework",
-                        "images": [
-                            "http://www.medpireland.com/communities/3/004/011/419/023/images/4583491184.jpg"
-                        ]
-                    },
-                    {
-                        "title": "LTE Integration",
-                        "dates": "2013-10-01",
-                        "description": "Testing new LTE framework",
-                        "images": [
-                            "http://sertekmedia.com/wp-content/uploads/2014/09/project-management.jpg"
-                        ]
-                    }
-                ],
-
+    // Recoding projects related information
+    projects:[
+    {
+        title: "Volvo Auto Driving",
+        dates: "2010-05-01",
+        description: "Testing new auto driving framework",
+        images: [
+        "http://www.medpireland.com/communities/3/004/011/419/023/images/4583491184.jpg"
+        ]
+    },
+    {
+        title: "LTE Integration",
+        dates: "2013-10-01",
+        description: "Testing new LTE framework",
+        images: [
+        "http://sertekmedia.com/wp-content/uploads/2014/09/project-management.jpg"
+        ]
+    }
+    ],
+// function to append projects related information to html
     display: function (){
         $("#projects").append(HTMLprojectStart);
         projects.projects.forEach (function(element){
 
-        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", element.title);
-        $(".project-entry:last").append(formattedProjectTitle);
-        var formattedProjectDates = HTMLprojectDates.replace("%data%", element.dates);
-        $(".project-entry:last").append(formattedProjectDates);
-        var formattedProjectDescription = HTMLprojectDescription.replace("%data%", element.description);
-        $(".project-entry:last").append(formattedProjectDescription);
-        var formattedProjectImage = HTMLprojectImage.replace("%data%", element.images);
-        $(".project-entry:last").append(formattedProjectImage);
-    });
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", element.title);
+            $(".project-entry:last").append(formattedProjectTitle);
+            var formattedProjectDates = HTMLprojectDates.replace("%data%", element.dates);
+            $(".project-entry:last").append(formattedProjectDates);
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", element.description);
+            $(".project-entry:last").append(formattedProjectDescription);
+            var formattedProjectImage = HTMLprojectImage.replace("%data%", element.images);
+            $(".project-entry:last").append(formattedProjectImage);
+        });
     }
 };
 
 
 var bio = {
-    "name": "Dustin",
-    "role": "PHP Engineer",
-    "welcomeMessage": "Welcome to my page",
-    "contacts": {
-        "mobile": "18601163385",
-        "email": "liucong.haonan@gmail.com",
-        "github": "grhaonan",
-        "location": "Beijing"
-        },
-    "skills":["Project Management","Full Stack Development", "Data Analysis"],
-    "biopic": "http://2.images.southparkstudios.com/default/image.jpg?quality=0.8",
+    // Recoding bio related information
+    name: "Dustin Liu",
+    role: "PHP Engineer",
+    welcomeMessage: "Welcome to my page",
+    contacts: {
+        mobile: "18601163385",
+        email: "liucong.haonan@gmail.com",
+        github: "grhaonan",
+        location: "Beijing"
+    },
+    skills:["Project Management", "Full Stack Development", "Data Analysis"],
+    biopic: "http://2.images.southparkstudios.com/default/image.jpg?quality=0.8",
+    // function to append bio related information to html
     display: function(){
-
-        // Add Bio Info
-         var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
         $("#header").prepend(formattedRole);
         var formattedName = HTMLheaderName.replace("%data%", bio.name);
         $("#header").prepend(formattedName);
@@ -137,104 +136,104 @@ var bio = {
 
         $("#header").append(HTMLskillsStart);
         bio.skills.forEach(function(element){
-        var formattedSkill = HTMLskills.replace("%data%", element);
+            var formattedSkill = HTMLskills.replace("%data%", element);
             $("#skills").append(formattedSkill);
         });
     }
 };
 
 var education = {
-    "schools": [
-        {
-            "name": "BeiHang University",
-            "location": "Beijing, China",
-            "degree": "BA",
-            "dates": "2007",
-            "url": "www.beihang.edu",
-            "majors": [
-                "Telecommunication Engineering"
-            ]
-        },
-        {
-            "name": "Chalmers University Of Technology",
-            "location": "Gothenburg, Sweden",
-            "degree": "Master",
-            "dates": "2009",
-            "url": "www.chalmers.se",
-            "majors": [
-                "Telecommunication Engineering"
-            ]
-        }
+// Recoding school related information
+    schools: [
+    {
+        name: "BeiHang University",
+        location: "Beijing, China",
+        degree: "BA",
+        dates: "2003-2007",
+        url: "www.beihang.edu",
+        majors: [
+        "Telecommunication Engineering"
+        ]
+    },
+    {
+        name: "Chalmers University Of Technology",
+        location: "Gothenburg, Sweden",
+        degree: "Master",
+        dates: "2007-2009",
+        url: "www.chalmers.se",
+        majors: [
+        "Telecommunication Engineering"
+        ]
+    }
     ],
-    "onlineCourses":[
-        {
-            "title": "Frontend Developer NanoDegree",
-            "school": "Udacity",
-            "dates": "2017",
-            "url": "www.udacity.com"
-        }
-
+    onlineCourses:[
+    {
+        title: "Frontend Developer NanoDegree",
+        school: "Udacity",
+        dates: "2017",
+        url: "www.udacity.com"
+    }
     ],
+     // function to append school related information to html
     display:function(){
         $("#education").append(HTMLschoolStart);
         education.schools.forEach(function(element){
 
-        var formattedEducationName = HTMLschoolName.replace("%data%", element.name);
-        var formattedDegree = HTMLschoolDegree.replace("%data%", element.degree);
-        var formattedEducationNameAndDegree = formattedEducationName + formattedDegree;
-        $(".education-entry:last").append(formattedEducationNameAndDegree);
+            var formattedEducationName = HTMLschoolName.replace("%data%", element.name);
+            var formattedDegree = HTMLschoolDegree.replace("%data%", element.degree);
+            var formattedEducationNameAndDegree = formattedEducationName + formattedDegree;
+            $(".education-entry:last").append(formattedEducationNameAndDegree);
 
-        var formatteedDates = HTMLschoolDates.replace("%data%", element.dates);
-        $(".education-entry:last").append(formatteedDates);
+            var formatteedDates = HTMLschoolDates.replace("%data%", element.dates);
+            $(".education-entry:last").append(formatteedDates);
 
-        var formattedLocation = HTMLschoolLocation.replace("%data%", element.location);
-        $(".education-entry:last").append(formattedLocation);
+            var formattedLocation = HTMLschoolLocation.replace("%data%", element.location);
+            $(".education-entry:last").append(formattedLocation);
 
-        var formattedMajor = HTMLschoolMajor.replace("%data%", element.majors);
-        $(".education-entry:last").append(formattedMajor);
+            var formattedMajor = HTMLschoolMajor.replace("%data%", element.majors);
+            $(".education-entry:last").append(formattedMajor);
         });
-        // Adding Online course info
-
         $("#education").append(HTMLonlineClasses);
         $("#education").append(HTMLschoolStart);
         education.onlineCourses.forEach(function(element){
-        var formattedOnlineCourseTitle = HTMLonlineTitle.replace("%data%", element.title);
-        var formattedOnlineCourseSchool = HTMLonlineSchool.replace("%data%", element.school);
+            var formattedOnlineCourseTitle = HTMLonlineTitle.replace("%data%", element.title);
+            var formattedOnlineCourseSchool = HTMLonlineSchool.replace("%data%", element.school);
 
-        var formattedOnlineCourseTtileAndSchool = formattedOnlineCourseTitle + formattedOnlineCourseSchool;
+            var formattedOnlineCourseTtileAndSchool = formattedOnlineCourseTitle + formattedOnlineCourseSchool;
 
-        $(".education-entry:last").append(formattedOnlineCourseTtileAndSchool);
+            $(".education-entry:last").append(formattedOnlineCourseTtileAndSchool);
 
-        var formattedOnlineCourseDates = HTMLonlineDates.replace("%data%", element.dates);
-        $(".education-entry:last").append(formattedOnlineCourseDates);
+            var formattedOnlineCourseDates = HTMLonlineDates.replace("%data%", element.dates);
+            $(".education-entry:last").append(formattedOnlineCourseDates);
 
-        var formattedOnlineCourseUrl = HTMLonlineURL.replace("%data%", element.url);
-        $(".education-entry:last").append(formattedOnlineCourseUrl);
+            var formattedOnlineCourseUrl = HTMLonlineURL.replace("%data%", element.url);
+            $(".education-entry:last").append(formattedOnlineCourseUrl);
 
         });
     }
 };
 
+// Append google map
 var map = {
 
     display:function(){
         $("#mapDiv").append(googleMap);
     }
-}
+};
 
+//Displaying Bio Info
 bio.display();
 
-//Adding work record to html
+//Displaying Work Info
 work.display();
 
-// Adding Projects
-
+//Displaying Projects Info
 projects.display();
 
-// Adding Education
-
+//Displaying Education Info
 education.display();
 
+//Displaying Map Info
 map.display();
 
 
